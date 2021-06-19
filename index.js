2e6deb175cd0a07996122ef684aa19b2b59711b7
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-06-19 12:48:29 +0530

    feat:  added item rendered to html

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const addBtn=document.querySelector("#add");
 const input=document.querySelector("#list-input")
+const displayTable=document.querySelector("#list-table")
 
-const list=[];
+const list=[{title:"first",checked:true,id:Date.now()}];
 
 
 function addToList()
 {
     console.log("clicked");
     let newtask={
-        name:input.value,
+        title:input.value,
         checked:false,
         id:Date.now()
     }
     list.push(newtask)
     input.value="";
-
+    input.focus()
+    displayList(list);
 }
 
 
+function displayList()
+{
+    
+    let tableList=list.map(task=>renderListItem(task))
 
+    console.log(tableList)
+    displayTable.innerHTML=tableList.join("")
+}
+
+
+function renderListItem(itemToRender)
+{
+    return `<li>${itemToRender.title}</li>`;
+}
 
 
 addBtn.addEventListener("click",addToList)
