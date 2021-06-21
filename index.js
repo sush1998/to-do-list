8e6217d9f11e24a4d5cb7d4cf238e8f4a9d33f96
SUSHANT CHANDRASHEKHAR MAHALLE <mahallesushant1998@gmail.com>
2021-06-21 11:59:22 +0530

    refact : change state of task using checkbox

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addToList(taskName,prio)
     //console.log("clicked");
     let newtask={
         title:taskName,
-        checked:false,
+        completed:false,
         priority:prio,
         id:Date.now()
     }
@@ -26,7 +26,7 @@ function addToList(taskName,prio)
 
 function getCountofTask_Yet_toComplete(list)
 {
-    let number= list.reduce((acc,item)=>item.checked==false?acc+1:acc,0)
+    let number= list.reduce((acc,item)=>item.completed==false?acc+1:acc,0)
     count.innerHTML=number
     
 }
@@ -54,11 +54,17 @@ function deleteItem(itemToDelete)
 
 function changeCheck(itemToChange)
 {
-    console.log("Checked clicked");
-    //console.log(itemToChange)
-    //let updated=list.map(item=>item.id==itemToChange?item.checked=true:item.checked=false) 
+    console.log("change  clicked");
     let indexInList=list.findIndex(item=>item.id==itemToChange);
-    list[indexInList].checked=true;
+    if(list[indexInList].completed==false)
+    {
+        list[indexInList].completed=true;
+    }
+    else
+    {
+        list[indexInList].completed=false;
+    }
+    
     getCountofTask_Yet_toComplete(list);
     //console.log(list)
  
@@ -66,22 +72,22 @@ function changeCheck(itemToChange)
 
 function filterTask(filterType)
 {   
-    // let filteredList=list.filter(item=>item.checked===true);
+    // let filteredList=list.filter(item=>item.completed===true);
     // console.log(filteredList)
     // displayList(filteredList);
     console.log(filterType);
     switch(filterType)
     {
         case "completed":
-            let completedTask=list.filter(item=>item.checked===true);
+            let completedTask=list.filter(item=>item.completed===true);
             displayList(completedTask)
             break;
         case "in-progress":
-            let inProgressTask=list.filter(item=>item.checked===false);
+            let inProgressTask=list.filter(item=>item.completed===false);
             displayList(inProgressTask)
             break;
         case "all":
-            //let allTask=list.filter(item=>item.checked===false);
+            //let allTask=list.filter(item=>item.completed===false);
             displayList(list)
             break;
         default:
@@ -94,7 +100,7 @@ function renderListItem(itemToRender)
 {
     return `<li>${itemToRender.title}</li>
     <button class="delete-btn" id=${itemToRender.id} onclick=deleteItem(${itemToRender.id})>X</button>
-    <button class="checked-btn" id=${itemToRender.id} onclick=changeCheck(${itemToRender.id})>Check</button>`
+    <input type="checkbox" id="${itemToRender.id}" onclick=changeCheck(${itemToRender.id})>`
 }
 
 // function getPriority(priority)
